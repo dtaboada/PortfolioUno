fb6708c36e6f97ee9a9340f0dd786c3809fca19a
Diego Taboada <dsp_diego@hotmail.com>
2022-08-15 02:58:07 -0300

    limpieza de archivos no usados y codigo html

--- assets/js/google-maps.js
+++ assets/js/google-maps.js
@@ -1,6 +1,6 @@
 function initMap() {
     // Latitude and Longitude
-    var myLatLng = {lat: -6.207690, lng: 106.985270};
+    var myLatLng = {lat: -34.91623904954773, lng: -57.91200527141091};
 
     var map = new google.maps.Map(document.getElementById('google-maps'), {
         zoom: 17,
@@ -10,6 +10,7 @@ function initMap() {
     var marker = new google.maps.Marker({
         position: myLatLng,
         map: map,
-        title: 'South Jakarta, INA' // Title Location
+        title: 'Berisso, BS.AS.' // Title Location
     });
 }
+
